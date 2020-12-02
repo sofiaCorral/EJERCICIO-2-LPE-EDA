9aa700f6384028ff8f9a9049c04b1257614a0f99
sofiaCorral <71374206+sofiaCorral@users.noreply.github.com>
2020-12-02 23:51:14 +0100

    DataSet Kw Paises

--- PrimerDataSet.R
+++ PrimerDataSet.R
@@ -1,11 +1,33 @@
+
+#Importamos las librerias necesarioas####
 pacman::p_load(pacman,tm,SnowballC,tidyverse)
-dataset <- read.csv("http://www.catastro.minhafp.es/documentos/estadisticas/RUSTICA2019.xls", header = TRUE, sep = ',')
-dataset
 
-dataset[1,]
+#Cargamos el dataset####
+TSV <- read_tsv("https://docs.google.com/spreadsheets/d/e/2PACX-1vQYkDKzlmUDpcVD1YOAhnFjcmIRNgMTdMOI-kInTO1yGvNWRhqvRzNz0zB2YisO2jaIcJFpXxHSNnI0/pub?gid=1141698242&single=true&output=tsv")
+TSV
 
-DATOS <- read_csv("http://www.catastro.minhafp.es/documentos/estadisticas/PARCELAS2019.xls")
+#Imprimo solo la fila de españa####
+Nuevo <- TSV[TSV$País == "España", ]
+Nuevo
 
-dataSetGuada <- openxlsx::read.xlsx("InformeInstalaciones-02_12_2020.xls")
-  read_(file.choose())
-dataSetGuada
+#Imprimo solo los valores de Kw####
+fila <- TSV[, 2:19]
+fila
+
+#PRUEBAS VISUALIZACIÓN####
+
+#esta funciona
+ggplot(fila) +
+geom_point( size=2, shape=21, fill="white", colour="red") + 
+  theme_minimal()
+
+
+library(ggplot2)
+ggplot(TSV, aes(x=País, y=Total2019)) + 
+  #geom_line(colour="red")  + 
+  geom_point( size=2, shape=21, fill="white", colour="red") + 
+  theme_minimal()
+
+library(ggplot2)
+g <- ggplot(fila, aes(x=País, fill=Nuevo[1:,]))
+g + geom_density(alpha=0.8, colour="gray60") + xlim(c(0, 8))
