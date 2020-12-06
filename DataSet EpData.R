3ace0f197d2163fbc912abc0399feddd82a34e76
sofiaCorral <71374206+sofiaCorral@users.noreply.github.com>
2020-12-06 21:10:25 +0100

    Segundo dataSet

--- DataSet EpData.R
+++ DataSet EpData.R
@@ -1,7 +1,33 @@
+#Cargamos el dataSet####
 epData <- read.csv(file.choose(), header = TRUE, sep = ';')
 epData
 
+#Valores nulos de todo el dataSet####
+NulosepData <- sum(is.na(epData))
+NulosepData
+
+#Nos quedamos solo con los datos que nos importan####
+epData <- epData[1:54,]
+epData
+
+#Calculo el valor mayor de energia solar####
 max(epData$Solar)
-epData <- as.factor(epData)
-hist(x =epData, main = "Histograma de la tasa de fertilidad", 
-     xlab = "Tasa de fertilidad", ylab = "Frecuencia")
+
+#DataSet solo con variables numericas####
+solNum <- epData[,3:10]
+solNum
+
+#imprimo el 2018####
+epData2018 <- epData[epData$AÃ.o == "2018", ]
+epData2018
+
+#Solo selecciono 
+epData2018 <- epData[,3:10]
+epData2018
+
+#Grafico de sectores####
+pie(epData2018, clockwise=TRUE, main="Grafico de sectores 2018")
+
+#Histograma####
+hist(x =epData, main = "Histograma de epData", 
+     xlab = "valor de X", ylab = "Valor de Y")
