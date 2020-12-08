f78227ea1337f525b628cee47a3ff42b4108c3d2
Christian Sucuzhanay Arevalo <christiansucuzhanayarevalo@ChristiansMBP15.home>
2020-12-08 07:52:20 +0100

    fixed some bug published Species graph

--- Tercer DataSet.R
+++ Tercer DataSet.R
@@ -3,5 +3,6 @@ pacman::p_load(pacman,tidyverse)
 #Cargamos los datos####
 datosAnimales <- read_csv('cites_listings_2020-11-27 22_58_comma_separated.csv')
 #especies solo espania####
-only_es <- datosAnimales %>% filter(All_DistributionFullNames=='Spain')
+only_es <- datosAnilibramales %>% filter(All_DistributionFullNames=='Spain')
 only_es %>% ggplot( mapping = aes(y=Family))+geom_bar()
+
