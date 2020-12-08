ceef129c032e55e6217505a4fb18747d16590183
sofiaCorral <71374206+sofiaCorral@users.noreply.github.com>
2020-12-08 12:02:22 +0100

    DataSet animales

--- Tercer DataSet.R
+++ Tercer DataSet.R
@@ -1,8 +1,18 @@
 #Importamos las librerias necesarias####
-pacman::p_load(pacman,tidyverse)
-#Cargamos los datos####
-datosAnimales <- read_csv('cites_listings_2020-11-27 22_58_comma_separated.csv')
-#especies solo espania####
-only_es <- datosAnilibramales %>% filter(All_DistributionFullNames=='Spain')
-only_es %>% ggplot( mapping = aes(y=Family))+geom_bar()
+pacman::p_load(pacman,tm,SnowballC,tidyverse)
 
+#Cargamos los datos####
+datosAnimales <- read_csv(file.choose())
+datosAnimales
+
+#Creo una variable con la columna de los paises####
+#Al ser esta columna en la que se va a enfocar mi analisis, he preferido verla mejor
+ColumnaPaises <- datosAnimales[,"All_DistributionFullNames"]
+ColumnaPaises
+
+#especies solo espania####
+soloEspania <- datosAnimales %>% filter(All_DistributionFullNames=='Spain')
+soloEspania
+
+#Visualizacion diagrama de barras####
+soloEspania %>% ggplot( mapping = aes(y=Family))+geom_bar()
